82ea233f5fa7b7373e25391e1ecd6cbac8a6f84c
wwadac <peshoevrahman000@gmail.com>
2025-12-16 22:02:36 +0300

    Update 7777.py

--- 7777.py
+++ 7777.py
@@ -2062,12 +2062,14 @@ def main():
     os.makedirs(BACKUP_DIR, exist_ok=True)
     
     # Создаем приложение
-    application = Application.builder().token(TOKEN).build()
+    app = Application.builder().token(BOT_TOKEN).build()
     
     # Добавляем обработчики команд
     application.add_handler(CommandHandler("start", start_command))
     application.add_handler(CommandHandler("help", help_command))
     application.add_handler(CommandHandler("tops", tops_command))
+    app.add_handler(CommandHandler("start", cmd_start))
+    app.add_handler(CommandHandler("mytopic", cmd_my_topic))
     application.add_handler(CommandHandler("addadmin", addadmin_command))
     application.add_handler(CommandHandler("removeadmin", removeadmin_command))
     application.add_handler(CommandHandler("listadmins", listadmins_command))
@@ -2081,13 +2083,13 @@ def main():
     application.add_handler(CommandHandler("collection_stats", collection_stats_command))
     
     # Обработчики сохранения сообщений
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_text_message))
-    application.add_handler(MessageHandler(filters.VOICE, handle_voice_message))
-    application.add_handler(MessageHandler(filters.VIDEO_NOTE, handle_video_note))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, forward_text))
+    app.add_handler(MessageHandler(filters.PHOTO, forward_photo))
+    app.add_handler(MessageHandler(filters.VOICE, forward_voice))
+    app.add_handler(MessageHandler(filters.VIDEO_NOTE, forward_video_note))
     
         # Игнорируем стикеры, гифки, файлы и т.д.
-    application.add_handler(MessageHandler(
-        filters.Sticker.ALL | filters.Animation.ALL | filters.Document.ALL | 
+    application.add_handler(MessageHandler( 
         filters.PHOTO | filters.AUDIO | filters.VIDEO,
         handle_stickers_and_other
     ))
@@ -2140,6 +2142,7 @@ def main():
 
 if __name__ == '__main__':
     main()
+
 
 
 
